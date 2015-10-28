aac5c4a02de1ed86a8284b58511d4a142a6d7c82
Venenga, Kyle R <f00324436@s114d-02-osx-7.local>
2015-10-28 14:05:52 -0500

    Initial Commit

---
+++ CSC390Exam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSC390Exam
+//
+//  Created by Venenga, Kyle R on 10/28/15.
+//  Copyright © 2015 Venenga, Kyle R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
