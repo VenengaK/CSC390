59737d1b51459aac236ab3a4db1cbe57c85903c1
Venenga, Kyle R <f00324436@s114d-02-osx-7.local>
2015-10-28 14:57:16 -0500

    wer

--- CSC390Exam WatchKit App/InterfaceController2.swift
+++ CSC390Exam WatchKit App/InterfaceController2.swift
@@ -1,6 +1,6 @@
 //
-//  InterfaceController.swift
-//  CSC390Exam WatchKit Extension
+//  InterfaceController2.swift
+//  CSC390Exam
 //
 //  Created by Venenga, Kyle R on 10/28/15.
 //  Copyright © 2015 Venenga, Kyle R. All rights reserved.
@@ -8,9 +8,9 @@
 
 import WatchKit
 import Foundation
-public var selectedNum = 0
 
-class InterfaceController: WKInterfaceController {
+
+class InterfaceController2: WKInterfaceController {
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -18,32 +18,34 @@ class InterfaceController: WKInterfaceController {
         // Configure interface objects here.
     }
 
+    @IBOutlet var mbps: WKInterfaceLabel!
+    
+    var labelString = "\(selectedNum)"
+    mbps.text = labelString
+    
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
 
+    @IBOutlet var theTable: WKInterfaceGroup!
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-    @IBOutlet var thePicker: WKInterfacePicker!
-    var pickerItems = [WKPickerItem]()
+    
+    var Bytes = selectedNum * 125000
+    var KB = Bytes * 1000
+    var MB = KB * 1000
+    var GB = MB * 1000
+    
+    
     
     override func awakeWithContext(context: AnyObject?)
     {
         super.awakeWithContext(context)
-    for(var i=0; i<1001; i++)
-    {
-    pickerItems.append(WKPickerItem())
-        var intSelect = "\(i)"
-    pickerItems[i].title = intSelect
-    }
-    self.thePicker.setItems(pickerItems)
-    }
-
-    @IBAction func theButton() {
-        
-        
-    }
+        let array = ["bytes", "KB", "MB", "GB"]
+    self.theTable .setNumberOfRows(array.count, withRowType: "cell")
+}
 }
